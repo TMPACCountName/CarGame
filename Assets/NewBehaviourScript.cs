519f0130561daa4083d718820a6e52d5abfbf405
Oleja302 <wetemez@gmail.com>
2020-06-17 15:28:26 +0300

    Первый commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using EndlessCarChase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    
+
+    private float nextDamage;
+    public float DamDelay = 1;
+    public float damage = 1f;
+
+
+     void Start()
+    {
+        nextDamage = Time.time + DamDelay;
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (nextDamage <= Time.time)
+        {
+            nextDamage = Time.time + DamDelay;
+            other.GetComponent<Standard>().ChangeHealth(-damage);
+        }
+    }   
+}
---
+++ Assets/shrink.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shrink : MonoBehaviour
+{
+
+    public GameObject objectToScale;
+    public float growRate = -3f;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        
+
+            transform.localScale += new Vector3(0.1F, .1f, .1f) * growRate * Time.deltaTime;
+
+
+        
+
+
+           
+    }
+}
