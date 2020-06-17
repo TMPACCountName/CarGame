7894b236157b47cc971df7dba594fb09a91c11eb
Zhenya <Canimore@yandex.ua>
2020-06-17 13:15:18 +0300

    Add base project and new main menu

---
+++ Assets/ECCAssets/Scripts/ECCMusicSliderControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ECCMusicSliderControllerScript : MonoBehaviour {
+
+	public Slider slider;
+	public float sliderValue;
+
+	public AudioSource audioSource;
+	public float musicVolume;
+
+	void Start () {
+		audioSource.Play();
+		slider.value = PlayerPrefs.GetFloat("saveMusic", sliderValue);
+	}
+
+	void Update()
+	{
+		audioSource.volume = musicVolume;
+	}
+
+	public void changeSlider(float value)
+	{
+		sliderValue = value;
+		musicVolume = value;
+		PlayerPrefs.SetFloat("saveMusic", sliderValue);
+	}
+}
---
+++ Assets/ECCAssets/Scripts/ECCSoundsSliderControllerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ECCSoundsSliderControllerScript : MonoBehaviour {
+
+	public Slider slider;
+	public float sliderValue;
+
+	public AudioSource audioSource;
+	public float soundVolume;
+
+	void Start()
+	{
+		slider.value = PlayerPrefs.GetFloat("saveSound", sliderValue);
+	}
+
+	void Update()
+	{
+		audioSource.volume = soundVolume;
+	}
+
+	public void changeSlider(float value)
+	{
+		sliderValue = value;
+		soundVolume = value;
+		PlayerPrefs.SetFloat("saveSound", sliderValue);
+	}
+}
